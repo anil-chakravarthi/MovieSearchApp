2aa0b43141ff386a1f57bc9bdb957f81bab309b3
Dileep kumar Meesala <r170988@rguktrkv.ac.in>
2025-10-21 10:34:46 +0530

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import { Routes, Route } from "react-router-dom";
 import { MovieProvider } from "./contexts/MovieContext";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
